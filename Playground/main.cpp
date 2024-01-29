dce221c0bd43b4e069949dd51bbb236c7a1f651c
Ian <ianperesdarosa2004@gmail.com>
2024-01-29 17:16:58 -0300

    commit

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() {
   
   string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};  
 
-  for (int32_t : cars->length) {
-    cout << i << "\n";
+  for (int i = 0; i < 4; ++i) {
+    cout << cars[i] << "\n";
   }
 
   return 0;
