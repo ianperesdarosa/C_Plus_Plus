bce5a13777ce5d6768d0cd700df55b3040f5ae2e
Ian Peres <ianperesdarosa2004@gmail.com>
2024-06-08 08:59:32 -0300

    commit

--- Playground/app_12.cpp
+++ Playground/app_12.cpp
@@ -3,15 +3,8 @@ using namespace std;
 
 int main() {
     enum level {
-        LOW = 25,
-        MEDIUM = 60,
-        HIGH = 80
-    };
-
-    
-
-    if () {
-
+        LOW,
+        MEDIUM,
+        HIGH
     }
-
 }
