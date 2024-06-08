cb5eed11492255fb138edc967235c02bf4b7b168
Ian Peres <ianperesdarosa2004@gmail.com>
2024-06-08 00:01:43 -0300

    commit

---
+++ Playground/app_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int idade;
+
+    cout << "Nome Completo: ";
+    cin >> name;
+    cout << "Idade: ";
+    cin >> idade;
+    cout << "Nome Completo: " << name << " Idade: " << idade;
+
+    return 0;
+}
